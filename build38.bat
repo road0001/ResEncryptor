88d96180176957436cbb0d7dd026a7c77f91e7e3
JMRY <376509849@qq.com>
2024-10-16 15:00:42 +0800

    1.3.1 20241016
    - 使用Python3.8打包，支持Windows 7。
    - 修复全部使用默认配置时，报错的bug。

---
+++ build38.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Running scripts...
+Python38\python.exe randomGenerate.py ResEncryptor.py
+Python38\python.exe outputVersion.py
+echo Building Execute file...
+Python38\python.exe Python38\scripts\pyinstaller.exe -F -i icon.ico ResEncryptor.py
+pause
---
+++ install38.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing dependencies...
+Python38\python.exe Python38\scripts\pip.exe install loguru
+Python38\python.exe Python38\scripts\pip.exe install send2trash
+Python38\python.exe Python38\scripts\pip.exe install pycryptodomex
+Python38\python.exe Python38\scripts\pip.exe install pyinstaller
+pause
